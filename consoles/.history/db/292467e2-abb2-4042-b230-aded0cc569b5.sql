b7bd303a4b29e8d05934d5e6b9c5f343ce70f3a8
danish mehmood <danishmehmood981@gmail.com>
2022-10-16 19:02:25 +0500

    few changes made

---
+++ consoles/.history/db/292467e2-abb2-4042-b230-aded0cc569b5.sql
@@ -0,0 +1,49 @@
+insert into authors (name, username, email) values ('Dewey Lonnon', 'dlonnon0', 'dlonnon0@studiopress.com');
+;-- -. . -..- - / . -. - .-. -.--
+insert into authors (name, username, email) values ('Clo Lincey', 'clincey1', 'clincey1@toplist.cz');
+;-- -. . -..- - / . -. - .-. -.--
+insert into authors (name, username, email) values ('Hanan Tolchard', 'htolchard2', 'htolchard2@chron.com');
+;-- -. . -..- - / . -. - .-. -.--
+insert into authors (name, username, email) values ('Belvia McDonand', 'bmcdonand3', 'bmcdonand3@google.co.uk');
+;-- -. . -..- - / . -. - .-. -.--
+insert into authors (name, username, email) values ('Madlen Philliskirk', 'mphilliskirk4', 'mphilliskirk4@spiegel.de');
+;-- -. . -..- - / . -. - .-. -.--
+insert into authors (name, username, email) values ('Dana Reardon', 'dreardon5', 'dreardon5@istockphoto.com');
+;-- -. . -..- - / . -. - .-. -.--
+insert into authors (name, username, email) values ('Thomasa Macieja', 'tmacieja6', 'tmacieja6@github.com');
+;-- -. . -..- - / . -. - .-. -.--
+insert into authors (name, username, email) values ('Haleigh Lillow', 'hlillow7', 'hlillow7@mtv.com');
+;-- -. . -..- - / . -. - .-. -.--
+insert into authors (name, username, email) values ('Stan Rendbaek', 'srendbaek8', 'srendbaek8@examiner.com');
+;-- -. . -..- - / . -. - .-. -.--
+insert into authors (name, username, email) values ('Morgen Carwithan', 'mcarwithan9', 'mcarwithan9@hexun.com');
+;-- -. . -..- - / . -. - .-. -.--
+insert into authors (name, username, email) values ('Kleon Cuthbertson', 'kcuthbertsona', 'kcuthbertsona@si.edu');
+;-- -. . -..- - / . -. - .-. -.--
+insert into authors (name, username, email) values ('Hamel Barbara', 'hbarbarab', 'hbarbarab@sina.com.cn');
+;-- -. . -..- - / . -. - .-. -.--
+insert into authors (name, username, email) values ('Jilleen Greneham', 'jgrenehamc', 'jgrenehamc@house.gov');
+;-- -. . -..- - / . -. - .-. -.--
+insert into authors (name, username, email) values ('Kellyann Aujean', 'kaujeand', 'kaujeand@xing.com');
+;-- -. . -..- - / . -. - .-. -.--
+insert into authors (name, username, email) values ('Antonin Van der Baaren', 'avane', 'avane@wsj.com');
+;-- -. . -..- - / . -. - .-. -.--
+insert into authors (name, username, email) values ('Randi Gormley', 'rgormleyf', 'rgormleyf@va.gov');
+;-- -. . -..- - / . -. - .-. -.--
+insert into authors (name, username, email) values ('Enrica Phinnessy', 'ephinnessyg', 'ephinnessyg@domainmarket.com');
+;-- -. . -..- - / . -. - .-. -.--
+insert into authors (name, username, email) values ('Hali Aireton', 'hairetonh', 'hairetonh@imageshack.us');
+;-- -. . -..- - / . -. - .-. -.--
+insert into authors (name, username, email) values ('Rorie Prinne', 'rprinnei', 'rprinnei@intel.com');
+;-- -. . -..- - / . -. - .-. -.--
+insert into authors (name, username, email) values ('Magdalena Sunnucks', 'msunnucksj', 'msunnucksj@google.cn');
+;-- -. . -..- - / . -. - .-. -.--
+show databases;
+;-- -. . -..- - / . -. - .-. -.--
+show tables;
+;-- -. . -..- - / . -. - .-. -.--
+select * from book;
+;-- -. . -..- - / . -. - .-. -.--
+select * from books;
+;-- -. . -..- - / . -. - .-. -.--
+insert into books (author_id, book_name, pages, price) values (7, 'Salix alba L.', 743, 190.88);
---
+++ laravel-tinker/LaravelTinkerConsole_1.php
@@ -0,0 +1,2 @@
+<?php
+//Tinker away!
