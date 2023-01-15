38318905d52c87ac59b1142a9be35bda8971574c
danish mehmood <danishmehmood981@gmail.com>
2023-01-15 18:47:00 +0500

    added more settings, plugins, styles, font chaned for source code pro etc

--- laravel-tinker/LaravelTinkerConsole.php
+++ laravel-tinker/LaravelTinkerConsole.php
@@ -1,3 +1,10 @@
 <?php
 //Tinker away!
 
+
+
+\App\Models\Pen::updateOrCreate([
+   'name' => 'Sample Pen',
+   'weight' => 45.87,
+   'color' => 'orange'
+]);
--- laravel-tinker/LaravelTinkerConsole_1.php
+++
@@ -1,2 +0,0 @@
-<?php
-//Tinker away!
